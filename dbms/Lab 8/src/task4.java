096a4c1c1f1ab0f79a97fe29840cb258da58e96a
Arham Apon <arhamaponutsho@gmail.com>
2025-02-23 23:45:58 +0600

    lab 8 dbms correction

--- dbms/Lab 8/src/task4.java
+++ dbms/Lab 8/src/task4.java
@@ -1,64 +1,44 @@
 import java.sql.*;
+
 public class task4 {
+    public  void execute() throws SQLException {
+        ConnectToDB connectToDB = new ConnectToDB();
+        Connection con = connectToDB.connect();
+        String task4 = "SELECT A.A_ID, SUM(T.AMOUNT) AS total_transaction_amount " +
+                "FROM ACCOUNT A " +
+                "INNER JOIN TRANSACTIONS T ON A.A_ID = T.A_ID " +
+                "GROUP BY A.A_ID";
 
-    public void execute() throws SQLException {
-
-        ConnectToDB c = new ConnectToDB();
-        Connection con = c.connect();
-
-        String query1 = "SELECT " +
-                "    SUM(balance > 1000000 AND total_transactions > 5000000) AS CIP_count, " +
-                "    SUM(balance > 500000 AND balance <= 900000 " +
-                "        AND total_transactions > 2500000 AND total_transactions < 4500000) AS VIP_count, " +
-                "    SUM(balance < 100000 AND total_transactions < 1000000) AS OP_count, " +
-                "    SUM(NOT (balance > 1000000 AND total_transactions > 5000000) " +
-                "        AND NOT (balance > 500000 AND balance <= 900000 " +
-                "                 AND total_transactions > 2500000 AND total_transactions < 4500000) " +
-                "        AND NOT (balance < 100000 AND total_transactions < 1000000)) AS Others_count " +
-                "FROM ( " +
-                "    SELECT A_ID, " +
-                "           SUM(CASE WHEN TYPE = 0 THEN AMOUNT ELSE -AMOUNT END) AS balance, " +
-                "           SUM(AMOUNT) AS total_transactions " +
-                "    FROM TRANSACTIONS " +
-                "    GROUP BY A_ID " +
-                ") AS AccountSummary";
+        int cip = 0;
+        int vip = 0;
+        int op = 0;
+        int ctless = 0;
 
 
 
+        PreparedStatement pstmt = con.prepareStatement(task4);
+        ResultSet rs4 = pstmt.executeQuery();
 
+        while (rs4.next()) {
+            int a_id = rs4.getInt("A_ID");
+            double total_transaction_amount = rs4.getDouble("total_transaction_amount");
 
-        java.sql.PreparedStatement pstmt = con.prepareStatement(query1);
-
-
-
-
-        ResultSet r1 = pstmt.executeQuery();
-        if (!r1.isBeforeFirst()) {
-            System.out.println("ResultSet is empty.");
-        } else {
-            while (r1.next()) {
-                int cipCount = r1.getInt("CIP_count");
-                int vipCount = r1.getInt("VIP_count");
-                int opCount = r1.getInt("OP_count");
-                int othersCount = r1.getInt("Others_count");
-
-                System.out.println("CIP Count: " + cipCount);
-                System.out.println("VIP Count: " + vipCount);
-                System.out.println("OP Count: " + opCount);
-                System.out.println("Others Count: " + othersCount);
+            if (total_transaction_amount > 5000000) {
+                cip++;
+            } else if (total_transaction_amount < 4500000 && total_transaction_amount > 2500000) {
+                vip++;
+            } else if (total_transaction_amount < 100000) {
+                op++;
+            } else {
+                ctless++;
             }
         }
 
-
-
-
-
-
-
-
-    }
+        System.out.println("CIP: " + cip);
+        System.out.println("VIP: " + vip);
+        System.out.println("OP: " + op);
+        System.out.println("others " + ctless);
 
 
     }
-
-
+}
